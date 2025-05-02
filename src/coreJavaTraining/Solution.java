e94071fb6f57c00bede30afa4c943d05d714acfa
Rishabh Verma <rishabh.verma@landisgyr.com>
2025-05-02 17:06:22 +0530

    FirstCommit
    Change-Id: I931496784cbadb7295c0b4d5784e7455075f72a7

---
+++ src/coreJavaTraining/Solution.java
@@ -0,0 +1,27 @@
+package coreJavaTraining;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        if (s.isEmpty()) {
+        	System.out.println(0);
+        }
+        else {
+        String[] arr = s.trim().split("[^A-Za-z]+");
+        System.out.println(arr.length);
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        }
+        scan.close();
+    }
+}
+
