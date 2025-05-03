147d57d62ea10d22df136fad290a4bf9ed29b8fa
iamrishabhv <rishabhverma41@gmail.com>
2025-05-03 22:15:03 +0530

    Added support for 3 methods of concatenating two strings

--- src/coreJavaTraining/stringConcatenation.java
+++ src/coreJavaTraining/stringConcatenation.java
@@ -1,28 +1,46 @@
 //https://www.codechef.com/blogs/strings-in-java
+//Write a Java program to concatenate a given string to the end of another string.
 package coreJavaTraining;
 
 import java.util.Scanner;
 
 public class stringConcatenation {
 	
-	//Simple Method for Concatenation
-	public void stringConcat() {
+	//Method 1 for Concatenation using String Literal class
+	public void literalConcat() {
 		String a = "Hello";
 		String b = "World";
 		System.out.println(a + " " + b);
+	}
+	
+	//Method 2 to concatenate a given string to the end of another string
+	public void endConcate() {
+		Scanner sa = new Scanner(System.in);
+		Scanner sb = new Scanner(System.in);
+		String c = sa.nextLine();
+		String d = sb.nextLine();
+		System.out.printf("String 1: %s%n", c);
+		System.out.printf("String 2: %s%n", d);
+		System.out.printf("The concatenated string: %s %n", c.concat(" " + d));
+	}
+	
+	//Method 3 to concatenate two string using StringBuffer Class
+	public void bufferConcate() {
+		Scanner s = new Scanner(System.in);
+		Scanner r = new Scanner(System.in);
+		StringBuffer t = new StringBuffer(s.nextLine());
+		StringBuffer u = new StringBuffer(r.nextLine());
+		System.out.printf("Appended String is: %s %n",t.append(" " + u));
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		stringConcatenation sc = new stringConcatenation();	//Object created for calling method defined in public class
-		
-		//Below is the second method for concatenating two strings
-		Scanner s = new Scanner(System.in);
-		Scanner r = new Scanner(System.in);
-		StringBuffer t = new StringBuffer(s.nextLine());
-		StringBuffer u = new StringBuffer(r.nextLine());
-		sc.stringConcat();
-		System.out.println(t.append(" " + u));
+		stringConcatenation sd = new stringConcatenation();
+		stringConcatenation se = new stringConcatenation();
+		sc.literalConcat();
+		sd.endConcate();
+		se.bufferConcate();
 	}
 
 }
