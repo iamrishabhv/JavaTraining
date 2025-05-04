c0df121f52dbe0bd21195e8c9f9049bf7c4874f6
iamrishabhv <rishabhverma41@gmail.com>
2025-05-04 13:30:43 +0530

    Added Program to check if a given string ends with provided input

--- src/coreJavaTraining/specifiedSequence.java
+++ src/coreJavaTraining/specifiedSequence.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class specifiedSequence {
 	
-	//Method to check if specified chars are present in a given string in the same sequence
+	Scanner a = new Scanner(System.in);
+	Scanner b = new Scanner(System.in);
+	
+	//Method 1: to check if specified chars are present in a given string in the same sequence
 	public void specificSequence() {
-		Scanner a = new Scanner(System.in);
-		Scanner b = new Scanner(System.in);
 		String s = a.nextLine();
 		String t = b.nextLine();
 		System.out.printf("Original String: %s%n", s);
@@ -19,11 +20,43 @@ public class specifiedSequence {
 			System.out.println(false);
 		}
 	}
+	
+	//Method 2: Using String Buffer Class
+	public void specificStringBuffer() {
+		StringBuffer r = new StringBuffer(a.nextLine());
+		StringBuffer p = new StringBuffer(b.nextLine());
+		if(r.toString().contentEquals(p.toString())) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
+	}
+	
+	//Program to check whether a given string ends with another string.
+	public void endWith() {
+		Scanner c = new Scanner(System.in);
+		Scanner d = new Scanner(System.in);
+		String u = c.nextLine();
+		String v = d.nextLine();
+		System.out.printf("String 1: %s%n", u);
+		System.out.printf("String 2: %s%n", v);
+		if(u.endsWith(v)) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		specifiedSequence se = new specifiedSequence();
+		specifiedSequence sd = new specifiedSequence();
+		specifiedSequence sf = new specifiedSequence();
 		se.specificSequence();
+		sd.specificStringBuffer();
+		sf.endWith();
 	}
 
 }
