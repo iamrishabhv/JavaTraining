341fc7d892f15941aebfe7c7116696e72a68494f
Rishabh Verma <rishabhverma41@gmail.com>
2025-05-06 18:01:07 +0530

    Updated program to add multiple of 3 pyramid print

--- src/coreJavaTraining/pyramidPrint.java
+++ src/coreJavaTraining/pyramidPrint.java
@@ -7,8 +7,8 @@ public class pyramidPrint {
 		
 		//Pyramid Print -  Ascending Order
 		int l = 1;
-		for (int m=0; m<4; m++) {
-			for(int n=4; n>=4-m; n--) {
+		for (int m=0; m<5; m++) {			// for (int m=1; m<5; m++)
+			for(int n=4; n>=5-m; n--) {		//Another logic: for(int n=1; n<=m; n++)
 				System.out.print(l);
 				System.out.print("\t");
 				l++;
@@ -18,12 +18,33 @@ public class pyramidPrint {
 		
 		//Pyramid Printing - Descending Order
 		int k = 1;
-		for(int i=0; i<4; i++) {
+		for(int i=0; i<5; i++) {
 
-			for(int j=1; j<=4-i; j++) {
+			for(int j=1; j<=5-i; j++) {
 				System.out.print(k);
 				System.out.print("\t");
 				k++;
+			}
+			System.out.println();
+		}
+		
+		//Pyramid Printing
+		for(int s=1; s<=4; s++) {
+			for(int t=1; t<=s; t++) {
+				System.out.print(t);
+				System.out.print("\t");	
+			}
+			System.out.println();
+		}
+		
+		//Pyramid Printing - Multiples of 3
+		int u = 3;
+		int count = u;
+		for(int s=1; s<=4; s++) {
+			for(int t=1; t<=s; t++) {
+				System.out.print(count);
+				System.out.print("\t");
+				count  = count + u;
 			}
 			System.out.println();
 		}
