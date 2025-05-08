38e461778bff7968d17fe90869b8368b010c8ebc
Rishabh Verma <rishabhverma41@gmail.com>
2025-05-08 21:46:03 +0530

    Updated code to add 2 new programs

--- src/coreJavaTraining/loopPractice.java
+++ src/coreJavaTraining/loopPractice.java
@@ -42,17 +42,51 @@ public class loopPractice {
 		System.out.print("\n");
 		System.out.printf("The Sum of Natural Number upto %d terms: %d",n,count1);
 	}
+	
+	//Sum and Average of 10 Numbers from Keyboard
+	public void avgNo(){
+		int sum = 0;
+		float average = 0;
+		for(int a=1;a<=10;a++) {
+			Scanner scan = new Scanner(System.in);
+			System.out.print("\n");
+			System.out.printf("Number %d:", a);
+			int n = scan.nextInt();
+			sum = sum+n;
+			average = (float)sum/10;
+		}
+		System.out.print("\n");
+		System.out.printf("The Sum of 10 Number is: %d",sum);
+		System.out.print("\n");
+		System.out.printf("The Average of 10 Number is: %f",average);
+	}
 
+	//Display Cube of Numbers Up to an Integer
+	public void cube() {
+		System.out.printf("How many number you want the cubes of?5 %n");
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		for(int a=1;a<=x;a++) {
+			Scanner scan = new Scanner(System.in);
+			System.out.printf("Enter Number %d: ",a);
+			int n = scan.nextInt();
+			int b = n*n*n;
+			System.out.printf("Number is %d and cube of the %d is: %d%n", n, n, b);
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		loopPractice nN = new loopPractice();
 		loopPractice nNS = new loopPractice();
 		loopPractice nNNS = new loopPractice();
+		loopPractice an = new loopPractice();
+		loopPractice cu = new loopPractice();
 		nN.naturalNo();
 		nNS.naturalNoSum();
 		nNNS.nNaturalNo();
-
+		an.avgNo();
+		cu.cube();
 	}
 
 }
