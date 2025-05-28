f28650237a72fabc1a3403e546cec41f4f558f76
iamrishabhv <rishabhverma41@gmail.com>
2025-05-28 17:20:16 +0530

    Added Program for Swap Numbers

---
+++ src/coreJavaTraining/swapNumbers.java
@@ -0,0 +1,18 @@
+package coreJavaTraining;
+
+public class swapNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c;
+		c = a;
+		a = b;
+		b = c;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
