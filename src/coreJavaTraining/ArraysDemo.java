30f7d64e73448757bd768fe6c5dde4ad1b3e0de3
iamrishabhv <rishabhverma41@gmail.com>
2025-05-31 12:51:39 +0530

    Added Demo for Single/Multi Dimensional Array

---
+++ src/coreJavaTraining/ArraysDemo.java
@@ -0,0 +1,44 @@
+package coreJavaTraining;
+
+public class ArraysDemo {
+	
+	public void singleDimensional() {
+		int a[] = new int[5];	//This is how an Array is initialized in Java
+		a[0] = 1;			//Initialized Values on a index in an Array
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		a[4] = 5;
+		int b[] = {4,5,6,7,8,9};	//This is another way of defining the array in which the memory is allocated dynamically.		
+		for(int i=0; i<a.length; i++) {
+			System.out.println(a[i]);		//Prints the values available in Array on a index.
+		}
+	}
+	
+	public void multiDimensional() {
+		int a[] [] = new int[2] [3];
+		a[0] [0] = 2;
+		a[0] [1] = 3;
+		a[0] [2] = 5;
+		a[1] [0] = 4;
+		a[1] [1] = 3;
+		a[1] [2] = 6;
+		for(int i = 0; i<a.length; i++) {
+			for(int j = 0; j<a.length+1; j++) {
+				System.out.print(a[i][j]);
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArraysDemo sd = new ArraysDemo();
+//		sd.singleDimensional();
+		sd.multiDimensional();
+		
+		
+	}
+}
