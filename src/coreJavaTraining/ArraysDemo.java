769c0fa9cd716d6df620814777cc85392f9dcebc
iamrishabhv <rishabhverma41@gmail.com>
2025-05-31 13:18:39 +0530

    Added program to find smallest number in 3x3 matrix

--- src/coreJavaTraining/ArraysDemo.java
+++ src/coreJavaTraining/ArraysDemo.java
@@ -32,13 +32,27 @@ public class ArraysDemo {
 			System.out.println();
 		}
 	}
+	
+	public void interviewQ1() {
+		int abc[] [] = {{2,4,5},{3,4,7},{1,2,9}};
+		int min = abc[0][0];
+		for(int i = 0; i<abc.length; i++) {
+			for(int j = 0; j<abc.length; j++) {
+				if(abc[i][j] < min) {
+					min = abc[i][j];
+				}
+			}
+		}
+		System.out.println(min);		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		ArraysDemo sd = new ArraysDemo();
 //		sd.singleDimensional();
-		sd.multiDimensional();
+//		sd.multiDimensional();
+		sd.interviewQ1();
 		
 		
 	}
