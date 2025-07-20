b01d8a51e95cabcfe46fd31342b49aba5044617a
iamrishabhv <rishabhverma41@gmail.com>
2025-07-20 14:13:27 +0530

    Updated to code to match the one on HackerRank

--- src/coreJavaTraining/JavaArrayListPractice.java
+++ src/coreJavaTraining/JavaArrayListPractice.java
@@ -7,28 +7,27 @@ public class JavaArrayListPractice {
 
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
-    	System.out.print("Enter the Number of Elements: ");
     	int N = sc.nextInt();
-    	sc.nextLine();
-    	List L = new ArrayList<>();
-    	System.out.print("Enter the Array Elements: ");
+    	List<Integer> L = new ArrayList<>();
     	for(int i = 0; i<N; i++) {
-    		L.add(sc.nextLine());
+    		L.add(sc.nextInt());
     	}
-    	System.out.print("Enter the Number of Queries: ");
     	int Q = sc.nextInt();
-    	System.out.println("Insert");
-    	int x = sc.nextInt();
-    	int y = sc.nextInt();
-    	if(x >= 0 && x <= L.size()) {
-        	L.add(x, y);
+    	for(int i = 0; i<Q; i++) {
+    		String query = sc.next();
+    		int x = sc.nextInt();
+    		if(query.equals("Insert")) {
+    			int y = sc.nextInt();
+    			L.add(x,y);
+    		}
+    		else {
+    			L.remove(x);
+    		}
     	}
-    	System.out.println("Delete");
-    	int z = sc.nextInt();
-    	if(z >=0 && z<L.size()) {
-        	L.remove(z);
+    	sc.close();
+    	for(Integer num : L) {
+    		System.out.print(num + " ");
     	}
-    	System.out.println(L);
     	
     }
 }
